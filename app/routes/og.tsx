9c9cff9eb3956d75c985c602c4ad094648b38d31
pitu.bprzz <pitu.bprzz@gmail.com>
2024-11-02 15:20:02 +0000

    🖼️ add OG image and README

---
+++ app/routes/og.tsx
@@ -0,0 +1,28 @@
+// app/routes/og.tsx
+import React from 'react';
+
+const OGImage = () => {
+  return (
+    <div class="p-6 mt-6">
+    <div
+      style={{
+        width: '1200px',
+        height: '630px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#333', // Dark background
+        borderRadius: '30px', // Rounded corners
+        color: '#fff', // White text
+        fontFamily: 'Instrument Serif', // Serif font
+        fontSize: '120px', // Font size for "VAZ"
+        textAlign: 'center',
+      }}
+    >
+      VAZ
+    </div>
+    </div>
+  );
+};
+
+export default OGImage;
