e49d91016ca40c2a8279e58ec11a33c7788a9adc
mateooo1 <prsl333@proton.me>
2024-11-26 15:03:49 +0000

    🔄 palette

--- app/routes/feed.tsx
+++ app/routes/feed.tsx
@@ -66,15 +66,7 @@ const activity = [
 
 
 
-const moods = [
-    { name: 'Excited', value: 'excited', icon: FireIcon, iconColor: 'text-white', bgColor: 'bg-red-500' },
-    { name: 'Loved', value: 'loved', icon: HeartIcon, iconColor: 'text-white', bgColor: 'bg-pink-400' },
-    { name: 'Happy', value: 'happy', icon: FaceSmileIcon, iconColor: 'text-white', bgColor: 'bg-green-400' },
-    { name: 'Sad', value: 'sad', icon: FaceFrownIcon, iconColor: 'text-white', bgColor: 'bg-yellow-400' },
-    { name: 'Thumbsy', value: 'thumbsy', icon: HandThumbUpIcon, iconColor: 'text-white', bgColor: 'bg-blue-500' },
-    { name: 'I feel nothing', value: null, icon: XMarkIcon, iconColor: 'text-gray-400', bgColor: 'bg-transparent' },
-  ]
-
+const moods = [ 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
